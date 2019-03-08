e31818148bb4e2adf238842951dea6f1f5f54655
J.T. Robinson <jaittarius@hotmail.com>
2019-03-07 21:24:36 -0500

    Replaced card carousel with card grid for projects component

--- src/app/project-list.ts
+++ src/app/project-list.ts
@@ -5,7 +5,7 @@ export const ProjectList: Project[] =  [
         id: 1,
         name: 'Project 1',
         description: 'Project 1 description',
-        thumbnail: 'https://via.placeholder.com/290x240.png',
+        thumbnail: 'https://via.placeholder.com/150x100.png',
         tags: ['Angular', 'Bootstrap'],
         url: 'http://www.example.com/1'
     },
@@ -13,7 +13,7 @@ export const ProjectList: Project[] =  [
         id: 2,
         name: 'Project 2',
         description: 'Project 2 description',
-        thumbnail: 'https://via.placeholder.com/290x240.png',
+        thumbnail: 'https://via.placeholder.com/150x100.png',
         tags: ['Knockout', 'jQuery'],
         url: 'http://www.example.com/2'
     },
@@ -21,7 +21,7 @@ export const ProjectList: Project[] =  [
         id: 3,
         name: 'Project 3',
         description: 'Project 3 description',
-        thumbnail: 'https://via.placeholder.com/290x240.png',
+        thumbnail: 'https://via.placeholder.com/150x100.png',
         tags: ['Python', 'Django'],
         url: 'http://www.example.com/3'
     },
@@ -29,7 +29,7 @@ export const ProjectList: Project[] =  [
         id: 4,
         name: 'Project 4',
         description: 'Project 4 description',
-        thumbnail: 'https://via.placeholder.com/290x240.png',
+        thumbnail: 'https://via.placeholder.com/150x100.png',
         tags: ['Pandas', 'NumPy'],
         url: 'http://www.example.com/4'
     },
@@ -37,8 +37,16 @@ export const ProjectList: Project[] =  [
         id: 5,
         name: 'Project 5',
         description: 'Project 5 description',
-        thumbnail: 'https://via.placeholder.com/290x240.png',
+        thumbnail: 'https://via.placeholder.com/150x100.png',
         tags: ['R', 'GGplot'],
         url: 'http://www.example.com/5'
-    }
+    },
+    {
+        id: 6,
+        name: 'Project 6',
+        description: 'Project 6 description',
+        thumbnail: 'https://via.placeholder.com/150x100.png',
+        tags: ['Matplotlib', 'Tableau'],
+        url: 'http://www.example.com/6'
+    },
 ];
