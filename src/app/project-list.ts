259f3942ef445d9441337d7502fcda40cf7a3fe3
J.T. Robinson <jaittarius@hotmail.com>
2019-03-25 21:04:02 -0400

    Added development projects to projects component

--- src/app/project-list.ts
+++ src/app/project-list.ts
@@ -3,50 +3,66 @@ import { Project } from './project';
 export const ProjectList: Project[] =  [
     {
         id: 1,
-        name: 'Project 1',
-        description: 'Project 1 description',
-        thumbnail: 'https://via.placeholder.com/150x100.png',
+        name: 'Mobile Phone Store',
+        description: 'Storefront developed with Angular',
+        thumbnail: '/assets/images/screenshots/CELL-RWD-screenshot-thumb.png',
         tags: ['Angular', 'Bootstrap'],
-        url: 'http://www.example.com/1'
+        url: 'https://github.com/jtrob704/CELL-RWD/tree/master/Iteration%203'
     },
     {
         id: 2,
-        name: 'Project 2',
-        description: 'Project 2 description',
-        thumbnail: 'https://via.placeholder.com/150x100.png',
-        tags: ['Knockout', 'jQuery'],
-        url: 'http://www.example.com/2'
+        name: 'Arcade Game',
+        description: 'A Frogger clone\n\n\n\n\n',
+        thumbnail: '/assets/images/screenshots/arcade-game-screenshot-thumb.png',
+        tags: ['Javascript', 'jQuery'],
+        url: 'https://github.com/jtrob704/frontend-nanodegree-arcade-game'
     },
     {
         id: 3,
-        name: 'Project 3',
-        description: 'Project 3 description',
-        thumbnail: 'https://via.placeholder.com/150x100.png',
-        tags: ['Python', 'Django'],
-        url: 'http://www.example.com/3'
+        name: 'Baseball Data Analysis',
+        description: 'An analysis of baseball hitters\n',
+        thumbnail: '/assets/images/screenshots/baseball-tableau-screenshot-thumb.png',
+        tags: ['Data Science', 'Tableau'],
+        url: 'https://public.tableau.com/profile/j.t.robinson#!/vizhome/BaseballStoryv2_0/Story1'
     },
     {
         id: 4,
-        name: 'Project 4',
-        description: 'Project 4 description',
-        thumbnail: 'https://via.placeholder.com/150x100.png',
+        name: 'WeRateDogs Analysis',
+        description: 'An analysis of data from WeRateDogs',
+        thumbnail: '/assets/images/screenshots/data-wrangling-screenshot-thumb.png',
         tags: ['Pandas', 'NumPy'],
-        url: 'http://www.example.com/4'
+        url: 'https://github.com/jtrob704/Data-Wrangling-Project'
     },
     {
         id: 5,
-        name: 'Project 5',
-        description: 'Project 5 description',
-        thumbnail: 'https://via.placeholder.com/150x100.png',
+        name: 'Red Wine Analysis',
+        description: 'An analysis of red wine data',
+        thumbnail: '/assets/images/screenshots/red-wine-analysis-screenshot.png',
         tags: ['R', 'GGplot'],
-        url: 'http://www.example.com/5'
+        url: 'https://github.com/jtrob704/Red-Wine-Analysis---EDA-DAND'
     },
     {
         id: 6,
-        name: 'Project 6',
-        description: 'Project 6 description',
-        thumbnail: 'https://via.placeholder.com/150x100.png',
-        tags: ['Matplotlib', 'Tableau'],
-        url: 'http://www.example.com/6'
+        name: 'Developer Portfolio',
+        description: 'My developer portfolio',
+        thumbnail: '/assets/images/screenshots/portfolio-screenshot-thumb.png',
+        tags: ['Angular', 'jQuery'],
+        url: 'https://github.com/jtrob704/Portfolio-Angular7'
     },
+    {
+        id: 7,
+        name: 'Neighborhood Map',
+        description: 'a webapp that uses Google and Foursqaure API data',
+        thumbnail: '/assets/images/screenshots/neighborhood-map-screenshot-thumb.png',
+        tags: ['API', 'Knockout.js'],
+        url: 'https://github.com/jtrob704/neighborhood-map'
+    },
+    {
+        id: 8,
+        name: 'Feed Reader',
+        description: 'Unit testing for a feed reader app',
+        thumbnail: '/assets/images/screenshots/unit-testing-screenshot-thumb.png',
+        tags: ['Unit Testing', 'Jasmine'],
+        url: 'https://github.com/jtrob704/frontend-nanodegree-feedreader'
+    }
 ];
