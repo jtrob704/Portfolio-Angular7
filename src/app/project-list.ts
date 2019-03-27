5d211400e54391fc6689b5831234472b1c341cdb
J.T. Robinson <jaittarius@hotmail.com>
2019-03-27 16:48:18 -0400

    Corrected typos in projects component.  Minor styling updates

--- src/app/project-list.ts
+++ src/app/project-list.ts
@@ -4,7 +4,7 @@ export const ProjectList: Project[] =  [
     {
         id: 1,
         name: 'Mobile Phone Store',
-        description: 'Storefront developed with Angular',
+        description: 'A storefront developed with Angular',
         thumbnail: '/assets/images/screenshots/CELL-RWD-screenshot-thumb.png',
         tags: ['Angular', 'Bootstrap'],
         url: 'https://github.com/jtrob704/CELL-RWD/tree/master/Iteration%203'
@@ -12,7 +12,7 @@ export const ProjectList: Project[] =  [
     {
         id: 2,
         name: 'Arcade Game',
-        description: 'A Frogger clone\n\n\n\n\n',
+        description: 'A browser based Frogger clone',
         thumbnail: '/assets/images/screenshots/arcade-game-screenshot-thumb.png',
         tags: ['Javascript', 'jQuery'],
         url: 'https://github.com/jtrob704/frontend-nanodegree-arcade-game'
@@ -20,7 +20,7 @@ export const ProjectList: Project[] =  [
     {
         id: 3,
         name: 'Baseball Data Analysis',
-        description: 'An analysis of baseball hitters\n',
+        description: 'A data analysis of baseball hitters\n',
         thumbnail: '/assets/images/screenshots/baseball-tableau-screenshot-thumb.png',
         tags: ['Data Science', 'Tableau'],
         url: 'https://public.tableau.com/profile/j.t.robinson#!/vizhome/BaseballStoryv2_0/Story1'
@@ -28,7 +28,7 @@ export const ProjectList: Project[] =  [
     {
         id: 4,
         name: 'WeRateDogs Analysis',
-        description: 'An analysis of data from WeRateDogs',
+        description: 'A data analysis of data from WeRateDogs',
         thumbnail: '/assets/images/screenshots/data-wrangling-screenshot-thumb.png',
         tags: ['Pandas', 'NumPy'],
         url: 'https://github.com/jtrob704/Data-Wrangling-Project'
@@ -36,7 +36,7 @@ export const ProjectList: Project[] =  [
     {
         id: 5,
         name: 'Red Wine Analysis',
-        description: 'An analysis of red wine data',
+        description: 'A data analysis of red wine data',
         thumbnail: '/assets/images/screenshots/red-wine-analysis-screenshot.png',
         tags: ['R', 'GGplot'],
         url: 'https://github.com/jtrob704/Red-Wine-Analysis---EDA-DAND'
@@ -52,7 +52,7 @@ export const ProjectList: Project[] =  [
     {
         id: 7,
         name: 'Neighborhood Map',
-        description: 'a webapp that uses Google and Foursqaure API data',
+        description: 'A webapp that utilizes Google and Foursquare API data',
         thumbnail: '/assets/images/screenshots/neighborhood-map-screenshot-thumb.png',
         tags: ['API', 'Knockout.js'],
         url: 'https://github.com/jtrob704/neighborhood-map'
@@ -60,7 +60,7 @@ export const ProjectList: Project[] =  [
     {
         id: 8,
         name: 'Feed Reader',
-        description: 'Unit testing for a feed reader app',
+        description: 'Unit testing for a feed reader webapp',
         thumbnail: '/assets/images/screenshots/unit-testing-screenshot-thumb.png',
         tags: ['Unit Testing', 'Jasmine'],
         url: 'https://github.com/jtrob704/frontend-nanodegree-feedreader'
