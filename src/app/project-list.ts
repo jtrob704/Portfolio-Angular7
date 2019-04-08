ba62e5c5e7f55f9f75c2f85ed7865d5a55305752
J.T. Robinson <jaittarius@hotmail.com>
2019-04-08 19:49:16 -0400

    Updated screenshot URLs

--- src/app/project-list.ts
+++ src/app/project-list.ts
@@ -5,7 +5,7 @@ export const ProjectList: Project[] =  [
         id: 1,
         name: 'Mobile Phone Store',
         description: 'A storefront developed with Angular',
-        thumbnail: '/assets/images/screenshots/CELL-RWD-screenshot-thumb.png',
+        thumbnail: 'assets/images/screenshots/CELL-RWD-screenshot-thumb.png',
         tags: ['Angular', 'Bootstrap'],
         url: 'https://github.com/jtrob704/CELL-RWD/tree/master/Iteration%203'
     },
@@ -13,7 +13,7 @@ export const ProjectList: Project[] =  [
         id: 2,
         name: 'Arcade Game',
         description: 'A browser based Frogger clone',
-        thumbnail: '/assets/images/screenshots/arcade-game-screenshot-thumb.png',
+        thumbnail: 'assets/images/screenshots/arcade-game-screenshot-thumb.png',
         tags: ['Javascript', 'jQuery'],
         url: 'https://github.com/jtrob704/frontend-nanodegree-arcade-game'
     },
@@ -21,7 +21,7 @@ export const ProjectList: Project[] =  [
         id: 3,
         name: 'Baseball Data Analysis',
         description: 'A data analysis of baseball hitters\n',
-        thumbnail: '/assets/images/screenshots/baseball-tableau-screenshot-thumb.png',
+        thumbnail: 'assets/images/screenshots/baseball-tableau-screenshot-thumb.png',
         tags: ['Data Science', 'Tableau'],
         url: 'https://public.tableau.com/profile/j.t.robinson#!/vizhome/BaseballStoryv2_0/Story1'
     },
@@ -29,7 +29,7 @@ export const ProjectList: Project[] =  [
         id: 4,
         name: 'WeRateDogs Analysis',
         description: 'A data analysis of data from WeRateDogs',
-        thumbnail: '/assets/images/screenshots/data-wrangling-screenshot-thumb.png',
+        thumbnail: 'assets/images/screenshots/data-wrangling-screenshot-thumb.png',
         tags: ['Pandas', 'NumPy'],
         url: 'https://github.com/jtrob704/Data-Wrangling-Project'
     },
@@ -37,7 +37,7 @@ export const ProjectList: Project[] =  [
         id: 5,
         name: 'Red Wine Analysis',
         description: 'A data analysis of red wine data',
-        thumbnail: '/assets/images/screenshots/red-wine-analysis-screenshot.png',
+        thumbnail: 'assets/images/screenshots/red-wine-analysis-screenshot.png',
         tags: ['R', 'GGplot'],
         url: 'https://github.com/jtrob704/Red-Wine-Analysis---EDA-DAND'
     },
@@ -45,7 +45,7 @@ export const ProjectList: Project[] =  [
         id: 6,
         name: 'Developer Portfolio',
         description: 'My developer portfolio',
-        thumbnail: '/assets/images/screenshots/portfolio-screenshot-thumb.png',
+        thumbnail: 'assets/images/screenshots/portfolio-screenshot-thumb.png',
         tags: ['Angular', 'jQuery'],
         url: 'https://github.com/jtrob704/Portfolio-Angular7'
     },
@@ -53,7 +53,7 @@ export const ProjectList: Project[] =  [
         id: 7,
         name: 'Neighborhood Map',
         description: 'A webapp that utilizes Google and Foursquare API data',
-        thumbnail: '/assets/images/screenshots/neighborhood-map-screenshot-thumb.png',
+        thumbnail: 'assets/images/screenshots/neighborhood-map-screenshot-thumb.png',
         tags: ['API', 'Knockout.js'],
         url: 'https://github.com/jtrob704/neighborhood-map'
     },
@@ -61,7 +61,7 @@ export const ProjectList: Project[] =  [
         id: 8,
         name: 'Feed Reader',
         description: 'Unit testing for a feed reader webapp',
-        thumbnail: '/assets/images/screenshots/unit-testing-screenshot-thumb.png',
+        thumbnail: 'assets/images/screenshots/unit-testing-screenshot-thumb.png',
         tags: ['Unit Testing', 'Jasmine'],
         url: 'https://github.com/jtrob704/frontend-nanodegree-feedreader'
     }
