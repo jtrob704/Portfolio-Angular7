8a9aac399f93bafad7df29a7b7c20fc3841c9133
J.T. Robinson <jaittarius@hotmail.com>
2020-01-16 16:28:31 -0500

    Removed page transition animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgtUniversalModule } from '@ng-toolkit/universal';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { TransferHttpCacheModule } from '@nguniversal/common';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,7 +10,6 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -28,8 +26,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
     TransferHttpCacheModule,
     HttpClientModule,
-
-    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
